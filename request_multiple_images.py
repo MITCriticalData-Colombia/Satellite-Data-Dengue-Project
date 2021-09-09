12e4a94def8c2ff7575d94eecf01796916543125
sebasmos <ulsordonez@unicauca.edu.co>
2021-09-09 15:12:02 +0200

    update epiweek

--- request_multiple_images.py
+++ request_multiple_images.py
@@ -10,6 +10,7 @@ import datetime
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
+from datetime import timedelta
 sys.path.insert(0,'..') # https://stackoverflow.com/questions/714063/importing-modules-from-parent-folder 
 from sentinelhub import MimeType, CRS, BBox, SentinelHubRequest, SentinelHubDownloadClient, DataCollection, bbox_to_dimensions, DownloadRequest
 
@@ -25,119 +26,119 @@ def plot_image(image, factor=1.0, clip_range = None, **kwargs):
     ax.set_xticks([])
     ax.set_yticks([])
 
-# bboxfinder.com/#6.192609,-75.607738,6.301649,-75.538731 
+def multiple_stamps(betsiboka_coords_wgs84, start, end, year):
 
-betsiboka_coords_wgs84 =  [-75.607738,6.192609, -75.538731,6.301649]
+    CLIENT_ID = "5a0eee73-51af-4cee-b314-7169ce5798e5"
+    CLIENT_SECRET = "h@{#0;:P^Ol(xk04G#@Q%29r>?k2rrZ>IKg}4h:?"
 
-CLIENT_ID = "**********"
-CLIENT_SECRET = "**********"
+    config = SHConfig()
 
-config = SHConfig()
-
-if CLIENT_ID and CLIENT_SECRET:
-    config.sh_client_id = CLIENT_ID
-    config.sh_client_secret=CLIENT_SECRET
-
-if not config.sh_client_id or not config.sh_client_secret:
-    print("Warning! To use Process API, please provide the credentials (OAuth client ID and client secret).")
+    if CLIENT_ID and CLIENT_SECRET:
+        config.sh_client_id = CLIENT_ID
+        config.sh_client_secret=CLIENT_SECRET
     
-resolution = 10
-betsiboka_bbox = BBox(bbox=betsiboka_coords_wgs84, crs=CRS.WGS84)
-betsiboka_size = bbox_to_dimensions(betsiboka_bbox, resolution=resolution)
+    if not config.sh_client_id or not config.sh_client_secret:
+        print("Warning! To use Process API, please provide the credentials (OAuth client ID and client secret).")
 
-print(f'Image shape at {resolution} m resolution: {betsiboka_size} pixels')
+    resolution = 10
+    betsiboka_bbox = BBox(bbox=betsiboka_coords_wgs84, crs=CRS.WGS84)
+    betsiboka_size = bbox_to_dimensions(betsiboka_bbox, resolution=resolution)
 
-'''
-# Fast test 
-start = datetime.datetime(2006,12,31) #2020-06-01', '2020-06-3
-end = datetime.datetime(2018,12,29)
-years = 12
-n_chunks = 48 * years # num semanas/año
-tdelta = (end - start) / n_chunks
-edges = [(start + i*tdelta).date().isoformat() for i in range(n_chunks)]
-slots = [(edges[i], edges[i+1]) for i in range(len(edges)-1)]
-'''
-# REAL TEST: 
-start = datetime.datetime(2015,8,1) #2020-06-01', '2020-06-3
-end = datetime.datetime(2015,12,31)
-years = 1
-days = 30
-n_chunks =  8*4# num semanas/año
-tdelta = (end - start) / n_chunks
-edges = [(start + i*tdelta).date().isoformat() for i in range(n_chunks)]
-slots = [(edges[i], edges[i+1]) for i in range(len(edges)-1)]
+    print(f'Image shape at {resolution} m resolution: {betsiboka_size} pixels')
 
-print('Weekly time windows:\n')
-for slot in slots:
-    print(slot)
+    n_chunks = 53
 
-evalscript_true_color = """
+    tdelta =  timedelta(days = 7)
+    edges = [(start + i*tdelta).date().isoformat() for i in range(n_chunks)]
+    slots = [(edges[i], edges[i+1]) for i in range(len(edges)-1)]
 
+    print('Weekly time windows:\n')
+    for slot in slots:
+        print(slot)
+
+    evalscript_true_color = """
+
+        //VERSION=3
+        function setup() {
+            return {
+                input: [{
+                    bands: ["B02", "B03", "B04", "B05", "B06", "B07", "B08"]
+                }],
+                output: {
+                    bands: 7
+                }
+            };
+        }
+        function evaluatePixel(sample) {
+            return [sample.B08, 
+                    sample.B07, 
+                    sample.B06, 
+                    sample.B05, 
+                    sample.B04, 
+                    sample.B03, 
+                    sample.B02];
+        }
+    """
+    
+    evalscript_rgb ="""
     //VERSION=3
+    
     function setup() {
-        return {
-            input: [{
-                bands: ["B02", "B03", "B04", "B05", "B06", "B07", "B08"]
-            }],
-            output: {
-                bands: 7
-            }
-        };
+      return {
+        input: ["B02", "B03", "B04"],
+        output: { bands: 3 }
+      };
     }
+    
     function evaluatePixel(sample) {
-        return [sample.B08, 
-                sample.B07, 
-                sample.B06, 
-                sample.B05, 
-                sample.B04, 
-                sample.B03, 
-                sample.B02];
+      return [2.5 * sample.B04, 2.5 * sample.B03, 2.5 * sample.B02];
     }
-"""
+    """
+    '''Check if directory exists, if not, create it'''
     
-def get_true_color_request(time_interval):
-    return SentinelHubRequest(
-        data_folder='data/2016',
-        evalscript=evalscript_true_color,
-        input_data=[
-            SentinelHubRequest.input_data(
-                data_collection=DataCollection.SENTINEL2_L1C,
-                time_interval=time_interval,
-                mosaicking_order='leastCC'
-            )
-        ],
-        responses=[
-            SentinelHubRequest.output_response('default', MimeType.TIFF)
-        ],
-        bbox=betsiboka_bbox,
-        size=betsiboka_size,
-        config=config
-    )
+    # You should change 'test' to your preferred folder.
+    path = os.path.abspath(os.getcwd()) + "/data"  + year
+    CHECK_FOLDER = os.path.isdir(path)
+    # If folder doesn't exist, then create it.
+    if not CHECK_FOLDER:
+        os.makedirs(path)
+        print("created folder : ", path)
+    else:
+            print(path, "folder already exists.")
 
-# create a list of requests
-list_of_requests = [get_true_color_request(slot) for slot in slots]
-list_of_requests = [request.download_list[0] for request in list_of_requests]
+    def get_true_color_request(time_interval):
+        return SentinelHubRequest(
+            data_folder= 'data' + year,
+            evalscript=evalscript_rgb,
+            input_data=[
+                SentinelHubRequest.input_data(
+                    data_collection=DataCollection.SENTINEL2_L1C,
+                    time_interval=time_interval,
+                    mosaicking_order='leastCC'
+                )
+            ],
+            responses=[
+                SentinelHubRequest.output_response('default', MimeType.TIFF)
+            ],
+            bbox=betsiboka_bbox,
+            size=betsiboka_size,
+            config=config
+        )
 
-# download data with multiple threads
+    # create a list of requests
+    list_of_requests = [get_true_color_request(slot) for slot in slots]
+    list_of_requests = [request.download_list[0] for request in list_of_requests]
+    print("Ended downloading")
 
-data = SentinelHubDownloadClient(config=config).download(list_of_requests, max_threads=5)
+    # download data with multiple threads
 
-ncols = 4
-nrows = 3
+    data = SentinelHubDownloadClient(config=config).download(list_of_requests, max_threads=5)
 
-aspect_ratio = betsiboka_size[0] / betsiboka_size[1]
-subplot_kw = {'xticks': [], 'yticks': [], 'frame_on': False}
+    ncols = 4
+    nrows = 3
 
-'''
-fig, axs = plt.subplots(ncols=ncols, nrows=nrows, figsize=(5 * ncols * aspect_ratio, 5 * nrows),
-                        subplot_kw=subplot_kw)
+    aspect_ratio = betsiboka_size[0] / betsiboka_size[1]
+    subplot_kw = {'xticks': [], 'yticks': [], 'frame_on': False}
 
-for idx, image in enumerate(data):
-    ax = axs[idx // ncols][idx % ncols]
-    ax.imshow(np.clip(image * 2.5/255, 0, 1))
-    ax.set_title(f'{slots[idx][0]}  -  {slots[idx][1]}', fontsize=10)
-    
-plt.tight_layout()
-'''
+    #lot_image(data[3][:,:,:1], factor=1.0, cmap=plt.cm.Greys_r, vmin=0, vmax=120)
 
-plot_image(data[0][:,:,:1], factor=1.0, cmap=plt.cm.Greys_r, vmin=0, vmax=120)
